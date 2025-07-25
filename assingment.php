9ee7561e81e3c0d423e9fb3d5ee3bfd7616baee7
sawlawla <lawlasaw619@gmail.com>
2025-07-25 21:48:55 +0630

    mysecondcommit

--- assingment.php
+++ assingment.php
@@ -1,3 +1,28 @@
-<?php
-echo "Lets Do it";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+
+<body>
+    <form action = "result.php" method = "post">
+<label>Mobile no.:+95 -</label>
+<input type = "number" name = "phonenumber"><br><br>    
+<label>Email:</label>
+<input type = "email" name = "email"><br><br>
+<label>Password:</label>
+<input type = "password" name = "password"><br><br>
+<label>Gender:</label>
+<input type = "radio" name="gender" value = "male" > Male <input type = "radio" name="gender" value = "female" >Female<br><br>
+<label>Department:</label><br>
+<input type = "checkbox" name="department" value = "English department"> English 
+<input type = "checkbox" name="department" value = "Computer department">Computer
+<input type = "checkbox" name="department" value = "Business department" >Business<br><br>
+<input type = "submit" name = "submit" value = "Register">
+</form>
+
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,61 @@
+<?php
+if(isset($_POST["submit"])){
+    $phone_number=$_POST["phonenumber"];
+    $email =$_POST["email"];
+    $password =$_POST["password"];
+   
+    if(empty($phone_number)){
+        echo "Phone number require! <br><br>";
+  
+      
+    }
+    else{
+        echo "Phone number $phone_number <br><br>";
+        
+        
+        
+    }
+        if(empty($email)){
+        echo "Email require! <br><br>";
+  
+      
+    }
+    else{
+        echo "Email : $email <br><br>";
+        
+        
+        
+    }
+        if(empty($password)){
+        echo "Password require! <br><br>";
+  
+      
+    }
+    else{
+        echo "Password is set! <br><br>";
+        
+        
+        
+    }
+    if(isset($_POST["gender"])){
+        $gender = $_POST["gender"];
+        echo "$gender <br><br>";
+    }
+    else{
+        echo "Please chose the selection! <br><br>";
+    }
+        if(isset($_POST["department"])){
+        $department = $_POST["department"];
+        echo "You are form $department <br><br>";
+    }
+    else{
+        echo "Please chose the selection! <br><br>";
+    }
+    
+    
+}
+
+
+
+
+?>
