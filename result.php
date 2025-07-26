fa0aabf33fc9ef6aedc7e9f3e854a4f0c9ee369b
sawlawla <lawlasaw619@gmail.com>
2025-07-26 11:12:33 +0630

    my4thcommit

--- result.php
+++ result.php
@@ -1,60 +1,47 @@
 <h1>Form submission result</h1>
 <?php
 if(isset($_POST["submit"])){
-    $phone_number=$_POST["phonenumber"];
-    $email =$_POST["email"];
-    $password =$_POST["password"];
-   
-    if(empty($phone_number)){
-        echo "Phone number require! <br><br>";
-  
-      
-    }
-    else{
-        echo "Phone number $phone_number <br><br>";
-        
-        
-        
-    }
-        if(empty($email)){
-        echo "Email require! <br><br>";
-  
-      
-    }
-    else{
-        echo "Email : $email <br><br>";
-        
-        
-        
-    }
-        if(empty($password)){
-        echo "Password require! <br><br>";
-  
-      
-    }
-    else{
-        echo "Password is set! <br><br>";
-        
-        
-        
-    }
-    if(isset($_POST["gender"])){
-        $gender = $_POST["gender"];
-        echo "$gender <br><br>";
-    }
-    else{
-        echo "Please chose the selection! <br><br>";
-    }
-        if(isset($_POST["department"])){
-        $department = $_POST["department"];
-        echo "You are form $department <br><br>";
-    }
-    else{
-        echo "Please chose the selection! <br><br>";
-    }
+    $array = [
+        "phonenumber" => "Phone number",
+        "email" => "Email",
+        "password" => "Password",
+        "gender" => "Gender",
+        "department" => "Department"
+    ];
+    foreach ($array as $key => $value){
+        if(empty($_POST["$key"])){
+            echo "$value is require!<br><br>";
+        }
+        else{
+       if($key == "phonenumber"){
+        echo "Phonenumber : $_POST[$key] <br><br>";
+       }
+        elseif($key == "email"){
+        echo "Email : $_POST[$key] <br><br>";
+       }
+       elseif($key == "password"){
+        echo "$value is set <br><br>";
+       }
+       if($key == "gender"){
+        echo "$value : $_POST[$key] <br><br>";
+       }
+        if($key == "department"){
+        echo "$value : $_POST[$key] <br><br>";
+       }
     
-    
+        }
+
+    }
+
+
+
+
+
+
+
 }
+
+ 
 
 
 
