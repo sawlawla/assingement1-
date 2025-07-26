63cdab88864be532d674bbec2864c20f1e983f40
sawlawla <lawlasaw619@gmail.com>
2025-07-26 11:37:48 +0630

    my8thcommit

--- result.php
+++ result.php
@@ -1,37 +1,24 @@
 <h1>Form submission result</h1>
 <?php
-if(isset($_POST["submit"])){
+if (isset($_POST["submit"])) {
     $array = [
         "phonenumber" => "Phone number",
-        "email" => "Email",
-        "password" => "Password",
-        "gender" => "Gender",
-        "department" => "Department"
+        "email"       => "Email",
+        "password"    => "Password",
+        "gender"      => "Gender",
+        "department"  => "Department"
     ];
-    foreach ($array as $key => $value){
-        if(empty($_POST["$key"])){
-            echo "$value is require!<br><br>";
-        }
-        else{
-       if($key == "phonenumber"){
-        echo "$value : $_POST[$key] <br><br>";
-       }
-        elseif($key == "email"){
-        echo "$value : $_POST[$key] <br><br>";
-       }
-       elseif($key == "password"){
-        echo "$value is set <br><br>";
-       }
-       if($key == "gender"){
-        echo "$value : $_POST[$key] <br><br>";
-       }
-        if($key == "department"){
-        echo "$value : $_POST[$key] <br><br>";
-       }
-    
-        }
 
+    foreach ($array as $key => $value) {
+        if (empty($_POST[$key])) {
+            echo "$value is required!<br><br>";
+     } else {
+            if ($key == "password") {
+            echo "$value is set<br><br>";
+         } else {
+            echo "$value : " . $_POST[$key] . "<br><br>";
+        }
     }
-
-} 
+    }
+}
 ?>
